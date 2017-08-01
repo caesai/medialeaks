998e82436308b006f93fd395566ec0a4a8733eaa
Vasily Kharlamov <caesai@yandex.ru>
2017-08-01 12:52:12 +0300

    Desktop styles for template

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -230,7 +230,9 @@ export default class Template extends React.Component {
             <div className="didgest_news">
               <div className="didgest_image"></div>
               <div className="didgest_caption">
-                <p>Тираннозавр, зомби и взрывающаяся голова. Какие эмодзи ждут нас в будущем и как общаться ими уже сейчас</p>
+                <a href="#">
+                  <p>Тираннозавр, зомби и взрывающаяся голова. Какие эмодзи ждут нас в будущем и как общаться ими уже сейчас</p>
+                </a>
                 <div className="didgest_credential">
                   <p className="didgest_date">13 июля 2017 г.</p>
                   <p className="didgest_category">Видео</p>
@@ -241,7 +243,9 @@ export default class Template extends React.Component {
             <div className="didgest_news">
             <div className="didgest_image"></div>
               <div className="didgest_caption">
-                <p>«Улетаю с этой планеты». Паук так съел сверчка, что всем нам теперь хочется смотреть себе под ноги</p>
+                <a href="#">
+                  <p>«Улетаю с этой планеты». Паук так съел сверчка, что всем нам теперь хочется смотреть себе под ноги</p>
+                </a>
                 <div className="didgest_credential">
                   <p className="didgest_date">13 июля 2017 г.</p>
                   <p className="didgest_category">Видео</p>
@@ -254,7 +258,9 @@ export default class Template extends React.Component {
             <div className="history-ofaday">
               <span className="h3">История дня</span>
               <div className="history-ofaday_caption">
-                <p>Когда включили «Деспасито». Как песня музыкантов из Пуэрто-Рико стала хитом этого лета и превратилась в мем</p>
+                <a href="#">
+                  <p>Когда включили «Деспасито». Как песня музыкантов из Пуэрто-Рико стала хитом этого лета и превратилась в мем</p>
+                </a>
                 <div className="didgest_credential">
                   <p className="didgest_date">13 июля 2017 г.</p>
                   <p className="didgest_category">Видео</p>
