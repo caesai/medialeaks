bb3f38b4d970397b0d7f0f0bd56471065c2d070d
Vasily Kharlamov <caesai@yandex.ru>
2017-08-10 16:35:16 +0300

    Post List

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -19,7 +19,7 @@ class PostList extends React.Component {
     }
   }
   componentWillMount () {
-    return fetch(DEV_SITE + '/wp-json/wp/v2/posts',{
+    return fetch(DEV_SITE + '/wp-json/wp/v2/posts?_embed',{
       method: 'get',
       credentials: 'include',
       headers: {
@@ -29,6 +29,7 @@ class PostList extends React.Component {
     })
     .then(parseJSON)
     .then(posts => {
+      console.log(posts);
       this.setState({
         posts: posts
       });
@@ -36,22 +37,47 @@ class PostList extends React.Component {
     .catch((error) => console.log(error));
   }
   render(){
+    let posts = this.state.posts || [];
+    let postsRow = posts.map(function(item){
+      return (
+        <div className="post-news" key={item.id}>
+          <Link to={`/${item.id}`}>
+            <div className="post-news_image">
+            { item.featured_image }
+              {item._embedded['wp:featuredmedia'].map((media, key) => {
+                return <img src={media.source_url} alt="" key={key} />
+              })}
+            </div>
+            <div className="post-news_caption">
+              <p>{item.title.rendered}</p>
+            </div>
+          </Link>
+          <div className="post-news_credentials">
+            <p>{item.date}</p>
+            <p>
+              <a href="#">Видео</a>
+            </p>
+          </div>
+        </div>
+      )
+    }).reduce(function(r, element, index){
+      index % 3 === 0 && r.push([]);
+      r[r.length - 1].push(element);
+      return r;
+    }, []).map(function(rowContent, key) {
+        return <div className="list-row" key={key}>{rowContent}</div>;
+    });
     return(
-      <div>
-        <ul>
-          {
-            this.state.posts.map(item => (
-              <li key={item.id}>
-                <Link to={`/${item.id}`}>
-                  {item.title.rendered}
-                </Link>
-                {item.excerpt.rendered}
-                  {console.log(item)}
-              </li>
-            ))
-          }
-        </ul>
-        BlogList
+      <div className="main-container">
+        <div className="list-wrapper">
+          <div className="posts-list">
+            <h2>самое интересное в интернете сегодня</h2>
+            {postsRow}
+            <div className="more-posts_btn">
+              <span className="h3">загрузить еще</span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import PostList from '../containers/PostList';
 import PostPage from '../containers/PostPage';
 
 export default(
-  <Route path='/' component={ListTemplate}>
+  <Route path='/' component={MainView}>
     <IndexRoute component={PostList} />
     <Route path=':id' component={PostPage} />
   </Route>
